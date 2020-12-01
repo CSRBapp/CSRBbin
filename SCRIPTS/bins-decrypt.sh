bc20b4c1e96841d7300d0375741fa2deb7a781e5
CSRBdev <dev@csrb.app>
2020-12-01 15:52:07 +0000

    [SCRIPTS] Added bin encryption/decryption scripts.

---
+++ SCRIPTS/bins-decrypt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "> " PASSWORD
+PASSWORDHASH=`echo -n ${PASSWORD} | sha512sum | awk '{ print $1 }'`
+
+echo "PASSWORD: ${PASSWORD}"
+echo "DECRYPTION PASSWORD: ${PASSWORDHASH}"
+
+for libenc in *.so.enc
+do
+	lib=${libenc%.enc}
+	echo "Decrypting ${lib}"
+	openssl enc -d \
+		-aes-256-cbc -md sha512 -pbkdf2 \
+		-pass pass:${PASSWORDHASH} \
+		-in $libenc \
+		-out $lib
+done
