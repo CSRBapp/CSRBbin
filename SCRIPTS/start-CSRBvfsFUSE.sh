ff9dc8c54aa66b12d20a61aec66527097552922e
Peri <peri@srdi.org>
2022-06-20 15:46:56 +0100

    [SCRIPTS] Updated env.sh and start-CSRBvfsFUSE.sh.
    Former-commit-id: 8f38b1a08f8af3bc09a0361c7f288ca408bc9269

--- SCRIPTS/start-CSRBvfsFUSE.sh
+++ SCRIPTS/start-CSRBvfsFUSE.sh
@@ -4,12 +4,27 @@ source $(dirname $0)/os-detect.sh
 source $(dirname $0)/env.sh
 . $(dirname $0)/checks.sh
 
-COMMAND_TIMEOUT=${COMMAND_TIMEOUT:+,commandTimeoutMS=${COMMAND_TIMEOUT}}
 NODEID=${NODEID:+,nodeID=${NODEID}}
+
 STORAGE_PATH=${STORAGE_PATH:+,storagePath=${STORAGE_PATH}}
+
 VFS_MOUNTPOINT=${VFS_MOUNTPOINT:-/tmp/CSRBVFS}
 VFS_WORKERS_COUNT=${VFS_WORKERS_COUNT:+,vfsWorkersCount=${VFS_WORKERS_COUNT}}
+
+COMMAND_TIMEOUT=${COMMAND_TIMEOUT:+,commandTimeoutMS=${COMMAND_TIMEOUT}}
+COMMAND_TIMEOUT_RETRIES=${COMMAND_TIMEOUT_RETRIES:+,commandTimeoutRetries=${COMMAND_TIMEOUT_RETRIES}}
+
+ATTRS_TIMEOUT=${ATTRS_TIMEOUT:-30}
+
 TRACEIO_ENABLE=${TRACEIO_ENABLE:-0}
+
+# NOTE: libfuse_direct_io=0 uses pages for R/W in 4k chunks
+if [ -z "$NODIRECTIO" ]
+then
+        DIRECTIO=1
+else
+        DIRECTIO=0
+fi
 
 while [ ! -d "${VFS_MOUNTPOINT}" ]
 do
@@ -20,19 +35,17 @@ done
 
 BIN=${BINDIR}/CSRBvfsFUSE
 
-# NOTE: libfuse_direct_io=0 uses pages for R/W in 4k chunks
-
 ${BIN} -o \
 nodev,\
 noatime,\
 allow_other,\
 libfuse_max_read=1048576,\
 libfuse_max_write=1048576,\
-libfuse_max_readahead=1048576,\
-libfuse_max_background=64,\
-libfuse_attrs_timeout=5,\
+libfuse_max_readahead=0,\
+libfuse_max_background=32,\
+libfuse_attrs_timeout=${ATTRS_TIMEOUT},\
 libfuse_direct_io=1,\
-force_open_direct_io=1,\
+force_open_direct_io=${DIRECTIO},\
 traceio_enable=${TRACEIO_ENABLE},\
 bindHost=${BIND_IP},\
 bindPort=${BIND_PORT},\
@@ -47,6 +60,7 @@ enableMicropython\
 ${NODEID}\
 ${STORAGE_PATH}\
 ${COMMAND_TIMEOUT}\
+${COMMAND_TIMEOUT_RETRIES}\
 ${VFS_WORKERS_COUNT}\
  -f ${VFS_MOUNTPOINT}
 
